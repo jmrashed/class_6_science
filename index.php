7b99cda174142379715d4afc9f8fdd84c08b583e
jmrashed <jmrashed@gmail.com>
2017-08-16 15:26:39 +0600

    initial all

--- index.php
+++ index.php
@@ -119,6 +119,84 @@ fixed point, so all motion is relative to what is defined as a fixed point. In f
                 <p class="caption" style="text-align: center !important;"><span class="title">Figure 10.3.1 </span>Box moves from one point to another which is translational motion</p>  
                 </center>
           </div>
+
+          <div class="col-md-12">
+              <div style="border:1px solid orange; border-radius: 10px; padding: 10px">
+              
+                <ul style="list-style: none;"> <span style="color:green; font-weight: bold;">TASK:</span>
+                  <li>
+                    <p align="justify">  1) Collect one brick</p>
+                  </li> 
+                  <li>
+                    <p align="justify">  2) Keep the brick at a particular point A on the table at your classroom.</p>
+                  </li> 
+                  <li>
+                    <p align="justify">  3) Mark the front and back edge point of the brick by drawing line on the table with a chalk.</p>
+                  </li> 
+                  <li>
+                    <p align="justify"> 4) Now, push the object in a forward direction and take it to a particular distance.</p>
+                  </li> 
+                  <li>
+                    <p align="justify"> 5) Measure the distance between the point of front edge marked on the table to the new position of the point of front edge.</p>
+                  </li> 
+                  <li>
+                    <p align="justify">  6) Measure the distance between marked point and the new position of back edge point.</p>
+                       <center><img src="images/rectilinear_motion.png" width="auto" title="" alt="" style="padding: 10px;  " class="img img-responsive">
+                        <p class="caption" style="text-align: center !important;"><span class="title">Fig 10.3.2 </span> Rectilinear motion</p>   
+                       </center>
+                  </li> 
+              </ul> 
+              <p align="justify"> The conclusion would be that both the distances are the same. If you measure the distance of the middle point to the new middle point, distance will be the same.</p>
+              </div>
+              <p align="justify">Translational motion may be divided into two kinds: 1) Rectilinear motion 2) Curvilinear motion </p>
+
+          </div>
+          <div class="col-md-12">
+             <p style="color:#13007C; font-weight: bold;"> Rectilinear motion</p>
+             <div class="col-md-8">
+               <p align="justify">The Motion attained by a body so that every particle of a body follows a straight line path is defined as the motion which occurs in a straight line. Because rectilinear motion takes place in one straight line, examples include a train following a straight set of tracks, a baseball thrown in a gravity-free vacuum or a penny that someone drops from the top of a tall building, heading straight to the ground.
+               </p>
+             </div>
+             <div class="col-md-4">
+                <center><img src="images/rectilinear_motion2.png" width="auto" class="img img-responsive">
+                        <p class="caption" style="text-align: center !important;"><span class="title">Fig 10.3.2 </span> Rectilinear motion</p>   
+                       </center>
+             </div>
+
+          </div>
+          <div class="col-md-12">
+             <p style="color:#13007C; font-weight: bold;">Curvilinear motion</p>
+             <div class="col-md-8">
+               <p align="justify">Curvilinear motion is defined as motion that occurs when a particle travels along a curved path. Examples include motion of an ant in a bent or curved wire.</p>
+                <center><img src="images/Curvilinearmotion2.png" width="auto"   " class="img img-responsive">
+                        <p class="caption" style="text-align: center !important;"><span class="title">Fig 10.3.4 </span> Rectilinear motion</p>   
+                       </center>
+
+             </div>
+             <div class="col-md-4"> 
+                <center><img src="images/Curvilinearmotion1.png" width="auto" class="img img-responsive">
+                        <p class="caption" style="text-align: center !important;"><span class="title">Fig 10.3.3 </span> Rectilinear motion</p>   
+                       </center>
+             </div>
+
+          </div>
+          <div class="col-md-12">
+              <div style="border:1px solid orange; border-radius: 10px; padding: 10px">
+              
+                <ul style="list-style: none;"> <span style="color:green; font-weight: bold;">TASK:</span>
+                  <li>
+                    <p align="justify">1) Tell one of your friend to walk straight from one end to the other end of your class-room. This is rect linear motion. </p>
+                  </li> 
+                  <li>
+                    <p align="justify"> 2) Tell another friend to walk from one end to another point of your class-room in a zigzag way.        This is curvilinear motion. </p>
+                  </li> 
+                </ul>
+              </div>
+          </div>          
+
+
+          
+  
            
          
         </div> 
@@ -126,9 +204,49 @@ fixed point, so all motion is relative to what is defined as a fixed point. In f
      
     <div id="part6" class="parts part6"> 
        <div class="main-frame-container"> 
-          <h2 style="margin-top: 10px; color:#13007C">  </h2>
-          <p align="justify"> 
+
+          <h2 style="margin-top: 10px; color:#13007C">10.4 ROTATIONAL MOTION  </h2>
+          <div class="col-md-8">
+          <p align="justify"> Rotational motion deals with changes in angle (θ). An object will always rotate—that is, spin or move in a  circle—around an axis of rotation. The earth spins around on the axis that runs from the North Pole to the South  Pole, creating day and night; figure skaters spin around on the axis from their head to their feet. Every point of the object revolves round the centre in a circular path making the same angle with different  radius, this motion is called rotational motion.
           </p> 
+          </div>
+          <div class="col-md-4">
+              <center><img src="images/rotation_motion.png" width="auto" class="img img-responsive">
+                <p class="caption" style="text-align: center !important;"><span class="title"> Figure 10.4.1 </span>The earth rotates around its axis</p>   
+              </center>
+          </div>
+          <div class="col-md-8"> 
+          <p align="justify">Another example is a fan rotating. In the picture below the rotation of the blades of the rotating electric fan displaces point to the point A, point B to the point B and point C to the point C. All points of the fan do not follow the same path but every point of the fan revolves round the centre of the fan in a circular path making the same angle with different radius.</p>
+          </div>
+          <div class="col-md-4">
+              <center><img src="images/fan.png" width="auto" class="img img-responsive">
+                <p class="caption" style="text-align: center !important;"><span class="title"> Figure 10.4.1 </span>The earth rotates around its axis</p>   
+              </center>
+          </div>
+
+
+          <div class="col-md-12" style="border:1px solid orange; border-radius: 10px;">
+            <div style=" padding: 10px; width: 70%; float:left; ">
+              
+                <ul style="list-style: none;"> <span style="color:green; font-weight: bold;">TASK:</span>
+                  <li>
+                    <p align="justify">1) Go to the field of your school. </p>
+                  </li> 
+                  <li>
+                    <p align="justify"> 2) Strongly bore a pole of bamboo in the ground.</p>
+                  </li> 
+                  <li>
+                    <p align="justify">  3) Holding the pole tightly by one hand, continue to rotate around the pole</p>
+                  </li> 
+                </ul>
+                <p align="center"> This motion is an example of rotational motion</p>
+              </div> 
+              <div style="width: 30%; float: right"> 
+              <center><img src="images/man.png" width="auto" height="80%" class="img img-responsive">
+                <p class="caption" style="text-align: center !important;"><span class="title"> Figure 10.4.3 </span>Rotational Motion</p>   
+              </center>
+            </div> 
+          
         </div> 
      </div>  
 
